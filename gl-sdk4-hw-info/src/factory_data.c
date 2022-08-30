8c42d4cc2fbad55b832e2d736c07113a5ab8ae38
gl-luochongjun <luochongjun@gl-inet.com>
2022-08-30 15:07:38 +0800

    gl factory data support mmc

--- gl-sdk4-hw-info/src/factory_data.c
+++ gl-sdk4-hw-info/src/factory_data.c
@@ -5,7 +5,49 @@
 #include <linux/of_platform.h>
 #include <linux/etherdevice.h>
 #include <linux/mtd/mtd.h>
+#include <linux/blkdev.h>
 #include "gl-hw-info.h"
+
+#define SECT_SIZE (1<<9)
+#define TO_SECTOR(x) (x>>9)
+#define TO_OFFSET(x) (x & (SECT_SIZE - 1))
+
+static unsigned char *partition_read_block(struct block_device *bdev, sector_t from, Sector *sector)
+{
+    if (from >= get_capacity(bdev->bd_disk)) {
+        return NULL;
+    }
+    return read_dev_sector(bdev, from, sector);
+}
+
+static int partition_read(const char *part, u32 offset, void * dest,  int len)
+{
+    struct block_device *bdev;
+    unsigned char *buf;
+    Sector sector;
+    int ret = 0;
+    const fmode_t mode = FMODE_READ;
+
+    if((len+TO_OFFSET(offset)) > SECT_SIZE){
+	return -1;
+    }
+
+    bdev = blkdev_get_by_path(part, mode, NULL);
+    if(IS_ERR(bdev)){
+	return -1;
+    }
+    buf = partition_read_block(bdev, TO_SECTOR(offset), &sector);
+    if(!buf){
+	ret = -1;
+	goto out;
+    }
+    memcpy(dest,buf+TO_OFFSET(offset), len);
+    put_dev_sector(sector);
+
+out:
+    blkdev_put(bdev, mode);
+    return ret;
+}
 
 #ifdef CONFIG_MTD
 static int parse_mtd_value(struct device_node *np, const char *prop,
@@ -21,6 +63,9 @@ static int parse_mtd_value(struct device_node *np, const char *prop,
 
     if (!of_property_read_string_index(np, prop, 1, &offset_str))
         offset = simple_strtoul(offset_str, NULL, 0);
+
+    if(!strncmp(part,"/dev/mmc",8))
+	return partition_read(part, offset, dest, len);
 
     mtd = get_mtd_device_nm(part);
     if (IS_ERR(mtd))
