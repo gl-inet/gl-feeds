cdf6e21acf2e7bcf6008ec761780026b36197f92
guiping.wu <guiping.wu@gl-inet.com>
2025-01-14 17:11:19 +0800

    feat: no dts system to get device info #SDK-3265
    Support for obtaining device information on the x86 version
    create file:/lib/firmware/sn.data
    input device info, for expame:
    model=mt3000
    country_code=CN
    wan=eth0
    lan=eth1
    device_mac=xx:xx:xx:xx:xx:xx
    device_sn=****************
    device_ddns=*******
    
    Change-Id: Ieaeec15e7aad1d4452a1fb3ff4864263cf3f243b
    Signed-off-by: guiping.wu <guiping.wu@gl-inet.com>

--- gl-sdk4-hw-info/src/main.c
+++ gl-sdk4-hw-info/src/main.c
@@ -17,6 +17,10 @@
 #include <linux/etherdevice.h>
 #include <linux/mtd/mtd.h>
 #include "gl-hw-info.h"
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/string.h>
 
 struct glinet_hw_info gl_hw_info = {};
 
@@ -80,16 +84,84 @@ static struct platform_driver gl_hw_info_driver = {
     }
 };
 
+static int gl_find_hw_info(void)
+{
+    struct file *           file;
+    loff_t                  offset      = 0;
+    ssize_t                 bytes_read;
+    char *                  buffer;
+    char *                  token;
+    char *                  line;
+    int                     ret         = 0;
+
+    buffer = kmalloc(GL_DEVICE_BUFFER_SIZE, GFP_KERNEL);
+    if (!buffer) {
+        ret = -1;
+        goto out;
+    }
+
+    if (IS_ERR(file = filp_open(GL_DEVICE_FILE_PATH, O_RDONLY, 0))) {
+        ret = -1;
+        goto free;
+    }
+
+    bytes_read = kernel_read(file, buffer, GL_DEVICE_BUFFER_SIZE - 1, &offset);
+    if (bytes_read < 0) {
+        ret = -1;
+        goto close;
+    }
+    buffer[bytes_read] = '\0';
+
+    line = buffer;
+    while ((line = strsep(&buffer, "\n")) != NULL) {
+        if (strncmp(line, GL_DEVICE_MAC, sizeof(GL_DEVICE_MAC)-1) == 0) {
+            token = line + sizeof(GL_DEVICE_MAC)-1;
+            create_proc_node("device_mac", token);
+        } else if (strncmp(line, GL_DEVICE_SN, sizeof(GL_DEVICE_SN)-1) == 0) {
+            token = line + sizeof(GL_DEVICE_SN)-1;
+            create_proc_node("device_sn", token);
+        } else if (strncmp(line, GL_DEVICE_DDNS, sizeof(GL_DEVICE_DDNS)-1) == 0) {
+            token = line + sizeof(GL_DEVICE_DDNS)-1;
+            create_proc_node("device_ddns", token);
+        } else if (strncmp(line, GL_DEVICE_MODEL, sizeof(GL_DEVICE_MODEL)-1) == 0) {
+            token = line + sizeof(GL_DEVICE_MODEL)-1;
+            create_proc_node("model", token);
+        } else if (strncmp(line, GL_DEVICE_COUNTRY_CODE, sizeof(GL_DEVICE_COUNTRY_CODE)-1) == 0) {
+            token = line + sizeof(GL_DEVICE_COUNTRY_CODE)-1;
+            create_proc_node("country_code", token);
+        } else if (strncmp(line, GL_DEVICE_LAN, sizeof(GL_DEVICE_LAN)-1) == 0) {
+            token = line + sizeof(GL_DEVICE_LAN)-1;
+            create_proc_node("lan", token);
+        } else if (strncmp(line, GL_DEVICE_WAN, sizeof(GL_DEVICE_WAN)-1) == 0) {
+            token = line + sizeof(GL_DEVICE_WAN)-1;
+            create_proc_node("wan", token);
+        }
+    }
+
+close:
+    filp_close(file, NULL);
+free:
+    kfree(buffer);
+out:
+    return ret;
+}
+
 static int __init gl_hw_info_init(void)
 {
     int ret;
+    struct device_node *data_np = of_find_node_by_name(NULL, "gl-hw");
+
     ret = proc_init_gl_hw_info();
     if (ret)
         goto err_out;
-    ret = platform_driver_register(&gl_hw_info_driver);
+
+    if (data_np)
+        ret = platform_driver_register(&gl_hw_info_driver);
+    else
+        ret = gl_find_hw_info();
+
     if (ret)
         goto err_proc_exit;
-
 
     return ret;
 err_proc_exit:
@@ -100,7 +172,11 @@ err_out:
 
 static void __exit gl_hw_info_exit(void)
 {
-    platform_driver_unregister(&gl_hw_info_driver);
+    struct device_node *data_np = of_find_node_by_name(NULL, "gl-hw");
+
+    if (data_np)
+        platform_driver_unregister(&gl_hw_info_driver);
+
     proc_remove_gl_hw_info();
 }
 
