515c85eff6b5e3c320e5a9b1ebbe49db87a6fcac
Jianhui Zhao <jianhui.zhao@gl-inet.com>
2025-12-24 15:01:21 +0800

    feat: add `assoclist` for libiwinfo-lua54
    Change-Id: I8215eb1a25d04d70b096ea4f34e0edb0659999b5
    Signed-off-by: Jianhui Zhao <jianhui.zhao@gl-inet.com>

--- libiwinfo-lua54/src/iwinfo.c
+++ libiwinfo-lua54/src/iwinfo.c
@@ -108,9 +108,72 @@ done:
     return ret;
 }
 
+static int lua_iwinfo_assoclist(lua_State *L)
+{
+    const char *ifname = luaL_checkstring(L, 1);
+    const struct iwinfo_ops *iw = iwinfo_backend(ifname);
+    struct iwinfo_assoclist_entry *e;
+    char buf[IWINFO_BUFSIZE];
+    char macstr[18];
+    int i, j, len, freq;
+    int ret = 1;
+
+    if (!iw) {
+        lua_pushnil(L);
+        lua_pushfstring(L, "No such wireless device: %s", ifname);
+        return 2;
+    }
+
+    if (iw->assoclist(ifname, buf, &len) || len <= 0) {
+        ret = 2;
+        lua_pushnil(L);
+        lua_pushfstring(L, "No assoclist information available");
+        goto done;
+    }
+
+    lua_newtable(L);
+
+    for (i = 0; i < len; i += sizeof(struct iwinfo_assoclist_entry)) {
+        e = (struct iwinfo_assoclist_entry *)&buf[i];
+
+        sprintf(macstr, "%02x:%02x:%02x:%02x:%02x:%02x",
+                e->mac[0], e->mac[1], e->mac[2],
+                e->mac[3], e->mac[4], e->mac[5]);
+
+        lua_newtable(L);
+
+        lua_pushinteger(L, e->signal);
+        lua_setfield(L, -2, "signal");
+
+        lua_pushinteger(L, e->noise);
+        lua_setfield(L, -2, "noise");
+
+        lua_pushinteger(L, e->inactive);
+        lua_setfield(L, -2, "inactive");
+
+        lua_pushinteger(L, e->rx_packets);
+        lua_setfield(L, -2, "rx_packets");
+
+        lua_pushinteger(L, e->tx_packets);
+        lua_setfield(L, -2, "tx_packets");
+
+        if (e->thr) {
+            lua_pushinteger(L, e->thr);
+            lua_setfield(L, -2, "expected_throughput");
+        }
+
+        lua_setfield(L, -2, macstr);
+    }
+
+done:
+    iwinfo_finish();
+    return ret;
+}
+
 static const luaL_Reg funcs[] = {
     {"info", lua_iwinfo_info},
     {"freqlist", lua_iwinfo_freqlist},
+    {"assoclist", lua_iwinfo_assoclist},
     {NULL, NULL}
 };
 
